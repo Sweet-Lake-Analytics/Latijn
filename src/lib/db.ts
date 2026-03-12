1e0bea4c5aad535f46716ea85ed4cc9e8214d5e3
Chris Jacobs <chris@sl-analytics.com>
2026-03-12 12:19:37 +0100

    Updated AWS_REGION to APP_AWS_REGION to prevent naming conflict in amplify.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand } from "@
 import { UserData } from './types';
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.APP_AWS_REGION || 'us-east-1',
 });
 
 const docClient = DynamoDBDocumentClient.from(client);
